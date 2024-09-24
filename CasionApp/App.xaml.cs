3999c2d69638baad41d926e882623a634a72702e
Karim Shakirov <kkarimyc779@gmail.com>
2024-09-24 15:05:50 +0300

    Добавьте файлы проекта.

---
+++ CasionApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CasionApp.Models;
+using CasionApp.Models.MetaData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CasionApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static CasinoDBEntities DB = new CasinoDBEntities();
+        public static User contextUser;
+        public static Frame mainFrame;
+        public static Page mainPage;
+
+        App()
+        {
+            RegistrProvider<User, MetaUser>();
+        }
+
+        private void RegistrProvider<T, A>()
+        {
+            var provider = new AssociatedMetadataTypeTypeDescriptionProvider(typeof(T), typeof(A));
+            TypeDescriptor.AddProviderTransparent(provider, typeof(T));
+        }
+
+        
+    }
+}
---
+++ CasionApp/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using CasionApp.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasionApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProfilePage.xaml
+    /// </summary>
+    public partial class ProfilePage : Page
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+            DataContext = App.contextUser;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            LVTransaction.ItemsSource = App.DB.Transaction
+                .Where(x => x.UserId == App.contextUser.Id)
+                .ToList()
+                .OrderByDescending(x=>x.DataTime);
+
+            LVGames.ItemsSource = App.DB.Game
+                .Where(x => x.UserId == App.contextUser.Id)
+                .ToList()
+                .OrderByDescending(x => x.StartSession);
+        }
+
+        private void BEdit_Click(object sender, RoutedEventArgs e)
+        {
+            App.mainFrame.Navigate(new RegestrationPage(App.contextUser));
+        }
+
+        private void BTopUp_Click(object sender, RoutedEventArgs e)
+        {
+            new TransactionWindows().ShowDialog();
+            App.mainPage.DataContext = null;
+            App.mainPage.DataContext = App.contextUser;
+            Refresh();
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void BBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void BPrintTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            var printDialog = new PrintDialog();
+            if (printDialog.ShowDialog().GetValueOrDefault())
+            {
+                printDialog.PrintVisual(LVTransaction, "");
+            }
+            
+        }
+
+        private void BPrintGames_Click(object sender, RoutedEventArgs e)
+        {
+            var printDialog = new PrintDialog();
+            if (printDialog.ShowDialog().GetValueOrDefault())
+            {
+                printDialog.PrintVisual(LVGames, "");
+            }
+        }
+
+        private void BRemove_Click(object sender, RoutedEventArgs e)
+        {
+            var listTransaction = App.DB.Transaction.Where(x => x.UserId == App.contextUser.Id).ToList();
+            foreach (var item in listTransaction)
+            {
+                App.DB.Transaction.Remove(item);
+            }
+
+            var listGame = App.DB.Game.Where(x => x.UserId == App.contextUser.Id).ToList();
+            foreach(var game in listGame)
+            {
+                foreach (var roundGame in App.DB.RoundGame.Where(x=>x.GameId==game.Id).ToList())
+                    App.DB.RoundGame.Remove(roundGame);
+                App.DB.Game.Remove(game);
+            }
+
+            App.DB.User.Remove(App.contextUser);
+            App.DB.SaveChanges();
+            App.mainFrame.Navigate(new LoginPage());
+        }
+    }
+}
